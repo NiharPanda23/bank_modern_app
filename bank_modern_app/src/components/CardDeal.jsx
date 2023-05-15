c97539c452877a4e0fe86e90a18d8ff6392da281
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2023-05-15 12:48:29 +0530

    Business section completed

---
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -0,0 +1,16 @@
+import { card } from "../assets"
+import styles, {layout} from "../style";
+import Button from "./Button";
+
+
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className="">Find a better card deal in few easy steps.</h2>
+    </div>
+  </section>
+)
+
+
+export default CardDeal
